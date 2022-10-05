a34ab47d5ebb002dda195952dc333bb6b78a70f5
93294 <9329496@mail.ru>
2022-10-05 22:43:58 +0300

    Третий комит

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int i;
 
-
+        System.out.println("оправка к коду");
 
         System.out.println("Hello world!");
     }
